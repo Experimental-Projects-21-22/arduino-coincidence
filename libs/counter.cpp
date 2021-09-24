a756baf7c9dc4f93af36cc6978daa57b5bc22d61
Julian van Doorn <jvdoorn@antarc.com>
2021-09-24 18:22:41 +0200

    Optimize for 32 bit. Drop support for 16 bit.

--- libs/counter.cpp
+++ libs/counter.cpp
@@ -16,7 +16,6 @@
 
 #include "Arduino.h"
 #include "counter.h"
-#include <math.h>
 
 void CounterIC::set_data_pins(uint8_t pins[bus_size]) {
     /*  Data pins on SN74LV8154 for parallel output.
@@ -30,13 +29,6 @@ void CounterIC::set_data_pins(uint8_t pins[bus_size]) {
 }
 
 void CounterIC::init() {
-    //Single 32-bit counter mode requires specific pin configuration
-    if (mode == Mode::SINGLE && CLKBEN_pin != unspecified_pin) {
-        Serial.println(
-                "fatal error: From CounterIC::init() -- cannot define CLKBEN or RCOA pins for single counter mode.");
-        while (1);
-    }
-
     //Initialize Gate pins
     if (GAL_pin == unspecified_pin || GAU_pin == unspecified_pin || GBL_pin == unspecified_pin ||
         GBU_pin == unspecified_pin) {
@@ -61,25 +53,6 @@ void CounterIC::init() {
         Serial.println("fatal error: From CounterIC::init() -- register clock pin must be defined!");
     }
 
-    //Initialize overflow status pin
-    if (RCOA_pin != unspecified_pin) {
-        _overflow = true;
-        pinMode(RCOA_pin, INPUT_PULLUP);
-    } else {
-        _overflow = false;
-    }
-
-    //Initialize Clock B enable pin
-    if (CLKBEN_pin != unspecified_pin) {
-        _clkBenable = true;
-        _toggle = true;
-        pinMode(CLKBEN_pin, OUTPUT);
-        digitalWrite(CLKBEN_pin, LOW);
-    } else {
-        _clkBenable = true;
-        _toggle = false;
-    }
-
     for (int i = 0; i < bus_size; ++i) {
         if (data_pins[i] == unspecified_pin) {
             Serial.println("fatal error: From CounterIC::init() -- data pins (Y0-Y7) must be defined!");
@@ -87,81 +60,12 @@ void CounterIC::init() {
         }
     }
 
-    //Initialize CCLR pin
     if (CCLR_pin != unspecified_pin) {
-        _clear = true;
         pinMode(CCLR_pin, OUTPUT);
         digitalWrite(CCLR_pin, HIGH);
     } else {
-        _clear = false;
-    }
-
-    //Initialize test pins
-    if (CLKA_pin != unspecified_pin) {
-        _testA = true;
-        pinMode(CLKA_pin, OUTPUT);
-        digitalWrite(CLKA_pin, LOW);
-        testA_delay = (uint32_t) round((1.0 / a_freq) * 1E6);
-        _resetTimers = true;
-    } else {
-        _testA = false;
-    }
-    if (CLKB_pin != unspecified_pin) {
-        _testB = true;
-        pinMode(CLKB_pin, OUTPUT);
-        digitalWrite(CLKB_pin, LOW);
-        testB_delay = (uint32_t) round((1.0 / b_freq) * 1E6);
-        _resetTimers = true;
-    } else {
-        _testB = false;
-    }
-}
-
-void CounterIC::update() {
-    /*  This function must be called in the main loop if testing of Counters
-     *  A/B is being performed.  It can also be used to monitor for overflow condition
-     *  on Counter A.  Alternatively, overFlow() function can be called for this purpose
-     */
-    _updateRunning = true;
-    tf_2 = micros();
-    tf_3 = micros();
-
-
-    //Handle timers for TestA and TestB signals
-    if (_testA || _testB) {
-        if (_resetTimers) {
-            t0_2 = micros();
-            t0_3 = micros();
-            tf_2 = micros();
-            tf_3 = micros();
-            _resetTimers = false;
-        }
-    }
-
-    if (_testA) {
-        if (_resetTimer2) {
-            t0_2 = micros();
-            tf_2 = micros();
-            _resetTimer2 = false;
-        }
-
-        if (tf_2 - t0_2 >= testA_delay / 2) {
-            digitalWrite(CLKA_pin, !digitalRead(CLKA_pin));
-            _resetTimer2 = true;
-        }
-    }
-
-    if (_testB) {
-        if (_resetTimer3) {
-            t0_3 = micros();
-            tf_3 = micros();
-            _resetTimer3 = false;
-        }
-
-        if (tf_3 - t0_3 >= testB_delay / 2) {
-            digitalWrite(CLKB_pin, !digitalRead(CLKB_pin));
-            _resetTimer3 = true;
-        }
+        Serial.println("fatal error: From CounterIC::init() -- CCLR pin must be defined!");
+        while (1);
     }
 }
 
@@ -175,13 +79,6 @@ uint32_t CounterIC::readCounter(Register reg) {
      *  SN74LV8154 by default.
      */
     uint32_t data_out = 0x00;
-
-    if (_testA || _testB) {
-        if (!_updateRunning) {
-            Serial.println("fatal error: From CounterIC::readCounter() -- CounterIC::Update() must be called in loop");
-            while (1);
-        }
-    }
 
     digitalWrite(RCLK_pin, HIGH);
     delayMicroseconds(2);
@@ -199,10 +96,6 @@ uint32_t CounterIC::readCounter(Register reg) {
         data_out = (data_out << 8) | readDataPins();
         digitalWrite(GAL_pin, HIGH);
     } else if (reg == Register::B) {
-        if (!_clkBenable) {
-            Serial.println("warning: From CounterIC::readCounter() -- counter B is not enabled, cannot read.");
-            return data_out;
-        }
         digitalWrite(GBU_pin, LOW);
         delayMicroseconds(2);
         data_out = readDataPins();
@@ -223,50 +116,10 @@ uint32_t CounterIC::readCounter_32bit() {
      *  by either connecting the CLKBEN pin to the RCOA pin or toggling Counter B on using this
      *  library when an overflow occurs on Counter A.
      */
-    uint32_t ret = 0xFFFF;
-    if (mode == Mode::SINGLE) {
-        uint32_t high_byte = readCounter(Register::A);
-        uint32_t low_byte = readCounter(Register::B);
-        ret = (high_byte << 16) | low_byte;
-    } else {
-        Serial.println(
-                "error: From CounterIC::readCounter_32bit() -- this function cannot be called for dual 16-bit counters configuration");
-    }
+    uint32_t low_byte = readCounter(Register::A);
+    uint32_t high_byte = readCounter(Register::B);
 
-    return ret;
-}
-
-void CounterIC::toggleCounterB() {
-    /*  Toggles the state of the CLKBEN pin on the SN74LV8154
-     *
-     */
-    if (_toggle) {
-        digitalWrite(CLKBEN_pin, !digitalRead(CLKBEN_pin));
-        if (digitalRead(CLKBEN_pin) == HIGH) { _clkBenable = false; }
-        else { _clkBenable = true; }
-    } else {
-        Serial.println(
-                "error: From CounterIC::toggleCounterB() -- cannot toggle counter B since CLKBEN pin was not defined.");
-    }
-}
-
-bool CounterIC::enabledCounterB() {
-    /*  Returns ON/OFF status of Counter B
-     *
-     */
-    bool ret = false;
-
-    if (_toggle) {
-        if (digitalRead(CLKBEN_pin) == HIGH) {
-            ret = false;
-        } else {
-            ret = true;
-        }
-    } else {
-        Serial.println(
-                "error: From CounterIC::enabledCounterB() -- this function cannot be called because CLKBEN pin was not undefined");
-    }
-    return ret;
+    return (high_byte << 16) | low_byte;
 }
 
 void CounterIC::clearCounters() {
@@ -279,12 +132,10 @@ void CounterIC::clearCounters() {
 }
 
 uint32_t CounterIC::readDataPins() {
-    uint32_t val = 0x00;
     uint32_t data_out = 0x00;
 
     for (int i = 0; i < bus_size; ++i) {
-        val = digitalRead(data_pins[i]);
-        data_out = data_out | (val << i);
+        data_out = data_out | (digitalRead(data_pins[i]) << i);
     }
 
     return data_out;
--- libs/counter.h
+++ libs/counter.h
@@ -23,50 +23,32 @@ class CounterIC {
 public:
     static const uint8_t bus_size = 8;
     static const uint8_t unspecified_pin = 255;
-    
-    enum class Mode {
-        SINGLE,
-        DUAL
-    };
+
     enum class Register {
         A,
         B
     };
-    
-    const Mode mode;
 
-    // Gate A lower byte, active-low puts lower byte of counter A on the Y bus.
+    // Register A lower byte, active-low puts lower byte of counter A on the Y bus.
     uint8_t GAL_pin = unspecified_pin;
-    // Gate A upper byte, active-low puts upper byte of counter A on the Y bus.
+    // Register A upper byte, active-low puts upper byte of counter A on the Y bus.
     uint8_t GAU_pin = unspecified_pin;
 
-    // Gate B lower byte, active-low puts lower byte of counter A on the Y bus.
+    // Register B lower byte, active-low puts lower byte of counter A on the Y bus.
     uint8_t GBL_pin = unspecified_pin;
-    // Gate B upper byte, active-low puts upper byte of counter A on the Y bus.
+    // Register B upper byte, active-low puts upper byte of counter A on the Y bus.
     uint8_t GBU_pin = unspecified_pin;
 
     // Clock clear, asynchronous active-low clear for both counters
     uint8_t CCLR_pin = unspecified_pin;
     // Register Clock, rising edge stores counters into an internal storage register.
     uint8_t RCLK_pin = unspecified_pin;
-    // Clock B enable, active-low allows clocking for counter B; connect to RCOA for 32-bit counter.
-    uint8_t CLKBEN_pin = unspecified_pin;
 
     // Data output bits 0 - 7, data_pins[0] -> Y0 (LSB) - data_pins[7] -> Y7 (MSB).
     uint8_t data_pins[bus_size] = {unspecified_pin};
 
-    // Clock A, rising edge count clock - used for testing.
-    uint8_t CLKA_pin = unspecified_pin;
-    // Clock B, rising edge count clock - used for testing.
-    uint8_t CLKB_pin = unspecified_pin;
-
-    // Count frequency for A - used for testing.
-    uint32_t a_freq = 0;
-    // Count frequency for B - used for testing.
-    uint32_t b_freq = 0;
-
     // Constructor
-    explicit CounterIC(Mode mode) : mode(mode) {}
+    CounterIC() {};
 
     // Pin setting functions
     void set_data_pins(uint8_t pins[bus_size]);
@@ -74,43 +56,13 @@ public:
     // Initialization function
     void init();
 
-    // Update function
-    void update();
-
-    //Counter functions
-    uint32_t readCounter(Register reg);
-
     uint32_t readCounter_32bit();
 
     void clearCounters();
 
-    void toggleCounterB();
-
-    bool enabledCounterB();
-
-private:
-    bool _clkBenable = false;
-    bool _updateRunning = false;
-    bool _overflow = false;
-    bool _toggle = false;
-    bool _clear = false;
-    bool _testA = false;
-    bool _testB = false;
-    bool _resetTimers = false;
-    bool _resetTimer1 = false;
-    bool _resetTimer2 = false;
-    bool _resetTimer3 = false;
-    uint32_t t0_1 = 0;
-    uint32_t t0_2 = 0;
-    uint32_t t0_3 = 0;
-    uint32_t tf_1 = 0;
-    uint32_t tf_2 = 0;
-    uint32_t tf_3 = 0;
-    uint32_t testA_delay = 0;
-    uint32_t testB_delay = 0;
-
     uint32_t readDataPins();
 
+    uint32_t readCounter(Register reg);
 };
 
 #endif /* COUNTER_H_ */
